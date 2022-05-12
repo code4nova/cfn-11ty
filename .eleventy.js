5d20ebf38abd6b8a5d25cebba424162d9118ff7e
Roy Mosby <ego.madking@gmail.com>
2022-05-12 19:50:51 -0400

    init commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig){
+
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPassthroughCopy('src/icons');
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/styles');
+  eleventyConfig.addPassthroughCopy('src/scripts');
+
+  return {
+    dir:{
+      input: "./src",
+      includes: "_includes",
+      layouts: "_layouts"
+    }
+  }
+}
